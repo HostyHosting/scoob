c3072bd516f0a92a2f0d078fa1cb0893d57e1b98
Jordan Gensler <jgensler@netflix.com>
2021-05-18 13:43:54 -0700

    Add tests to CI

--- src/modify.rs
+++ src/modify.rs
@@ -3,61 +3,65 @@ use colored::*;
 use std::env;
 
 enum Mode {
-	Create,
-	Edit,
+    Create,
+    Edit,
 }
 
 pub fn modify(cmd: &Modify) -> Result<(), &'static str> {
-	if env::var("EDITOR").is_err() || env::var("EDITOR").unwrap_or_else(|_| "".to_string()).is_empty() {
-		return Err(
+    if env::var("EDITOR").is_err()
+        || env::var("EDITOR")
+            .unwrap_or_else(|_| "".to_string())
+            .is_empty()
+    {
+        return Err(
 			"You must define your $EDITOR environment variable to modify a Scoob configuration file.",
 		);
-	}
+    }
 
-	if cmd.create && cmd.edit {
-		return Err("Both '--edit' and '--create' flags cannot be provided");
-	}
+    if cmd.create && cmd.edit {
+        return Err("Both '--edit' and '--create' flags cannot be provided");
+    }
 
-	if !cmd.create && !cmd.edit {
-		println!("{}", "Neither create nor edit mode was provided. Scoob will attempt to automatically determine the correct mode.".yellow());
-	}
+    if !cmd.create && !cmd.edit {
+        println!("{}", "Neither create nor edit mode was provided. Scoob will attempt to automatically determine the correct mode.".yellow());
+    }
 
-	if cmd.create && Config::exists(&cmd.file) {
-		return Err("The create flag was provided, but the secrets file already exists.");
-	}
+    if cmd.create && Config::exists(&cmd.file) {
+        return Err("The create flag was provided, but the secrets file already exists.");
+    }
 
-	if cmd.edit && !Config::exists(&cmd.file) {
-		return Err("The edit flag was provided, but the secrets file does not exist.");
-	}
+    if cmd.edit && !Config::exists(&cmd.file) {
+        return Err("The edit flag was provided, but the secrets file does not exist.");
+    }
 
-	let mode: Mode = if cmd.create || !Config::exists(&cmd.file) {
-		Mode::Create
-	} else {
-		Mode::Edit
-	};
+    let mode: Mode = if cmd.create || !Config::exists(&cmd.file) {
+        Mode::Create
+    } else {
+        Mode::Edit
+    };
 
-	let original_config = Config::get(&cmd.file);
-	let encryption = Encryption {
-		config: &original_config,
-	};
+    let original_config = Config::get(&cmd.file);
+    let encryption = Encryption {
+        config: &original_config,
+    };
 
-	let temp_file_contents = match mode {
-		Mode::Create => Config::default(),
-		Mode::Edit => original_config.with_placeholders(),
-	};
+    let temp_file_contents = match mode {
+        Mode::Create => Config::default(),
+        Mode::Edit => original_config.with_placeholders(),
+    };
 
-	let contents = edit::edit_with_builder(
-		serde_yaml::to_string(&temp_file_contents).unwrap(),
-		edit::Builder::new().suffix(".yml"),
-	);
+    let contents = edit::edit_with_builder(
+        serde_yaml::to_string(&temp_file_contents).unwrap(),
+        edit::Builder::new().suffix(".yml"),
+    );
 
-	let new_config: ConfigFile = serde_yaml::from_str(&contents.unwrap()).unwrap();
+    let new_config: ConfigFile = serde_yaml::from_str(&contents.unwrap()).unwrap();
 
-	let encrypted_config = encryption.encrypt_configuration(&new_config)?;
+    let encrypted_config = encryption.encrypt_configuration(&new_config)?;
 
-	std::fs::write(&cmd.file, serde_yaml::to_string(&encrypted_config).unwrap()).unwrap();
+    std::fs::write(&cmd.file, serde_yaml::to_string(&encrypted_config).unwrap()).unwrap();
 
-	println!("Wrote updated scoob configuration file at {:?}", cmd.file);
+    println!("Wrote updated scoob configuration file at {:?}", cmd.file);
 
-	Ok(())
+    Ok(())
 }
